27557522e054806080ad2497b6313bcacbbee688
MozMary <viktor83pochynok@gmail.com>
2023-05-20 20:36:51 +0300

    ready

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,37 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Container, Title, Subtitle } from 'components/App/App.styled';
 
 import { Form } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contactsLocalStorage = localStorage.getItem('contacts');
-    const parsContactsLocStorage = JSON.parse(contactsLocalStorage);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (parsContactsLocStorage) {
-      this.setState({ contacts: parsContactsLocStorage });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevContacts = prevState.contacts;
-    const currentContacts = this.state.contacts;
-
-    if (prevContacts !== currentContacts) {
-      localStorage.setItem('contacts', JSON.stringify(currentContacts));
-    }
-  }
-
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
+  const addContact = ({ name, number }) => {
     const normalizedName = name.toLocaleLowerCase();
     const findName = contacts.find(
       contact => contact.name.toLocaleLowerCase() === normalizedName
     );
 
     if (findName) {
-      alert(`${name} is already in the contacts`);
+      toast.error(`${name} is already in the contacts`);
       return;
     }
 
@@ -52,43 +40,37 @@ export class App extends Component {
       number,
       id: nanoid(),
     };
-    this.setState(prevState => ({ contacts: [...contacts, newContact] }));
+
+    setContacts(contacts => [...contacts, newContact]);
   };
 
-  changeFilter = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+  const changeFilter = event => {
+    setFilter(event.target.value);
   };
 
-  getVisibleContact = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContact = () => {
     const normalizedFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const { filter } = this.state;
-    const visibleContact = this.getVisibleContact();
+  const visibleContact = getVisibleContact();
 
-    return (
-      <Container>
-        <Title>Phonebook</Title>
-        <Form onSubmit={this.addContact} />
-        <Subtitle>Contacts</Subtitle>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          contacts={visibleContact}
-          onDeleteContact={this.deleteContact}
-        />
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <Form onSubmit={addContact} />
+      <Subtitle>Contacts</Subtitle>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList contacts={visibleContact} onDeleteContact={deleteContact} />
+      <ToastContainer theme="colored" autoClose={2000} position="top-right" />
+    </Container>
+  );
+};
